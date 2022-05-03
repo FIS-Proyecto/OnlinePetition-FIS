4942dbefa938d1de060becd974a029524705381c
Daniel Méndez <72448758+danielmdzrgz@users.noreply.github.com>
2022-05-03 10:19:54 +0100

    main.cpp fix
    ur mom gay lol

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 
 int main() {
   // Prints "Hello, World!" to the terminal window.
+  
   std::cout << "Hello, World!" << std::endl;
+  std::cout << "Your momma is so fat" << std::endl;
   return 0;
 }
