7c41d35066d279367e0d205491f72137448a560a
Adrián González Expósito <alu0101404813@ull.edu.es>
2022-05-13 19:48:46 +0100

    Function: firmar
    Se ha añadido la function firmar

---
+++ include/Logic/firmar.h
@@ -0,0 +1,16 @@
+#ifndef FIRMAR_H
+#define FIRMAR_H
+
+#include "petition.h"
+#include "user.h"
+
+void firmar_petit(block::petition_p& petition, const account::user& user) {
+  int current_sign = std::stoi(petition.data_.nSigns_);
+  petition.data_.nSigns_ = std::to_string(current_sign + 1);
+  db::query::edit_petition(petition.data_.get_pid(), db_edit::petition({
+     n_signs(petition.data_.nSigns_),
+  }));
+}
+
+
+#endif
--- src/Logic/login.cpp
+++ src/Logic/login.cpp
@@ -6,7 +6,6 @@
 bool login_function(std::string email, std::string passwd, account::user* user) {
   db::user db_user(db::query::get_user(email));
   std::string uid = std::to_string(std::hash<std::string_view>{}(email));
-  std::cout << uid << " -- db: " << db_user.get_uid() << "\n\n";
   if (db_user.get_uid() == uid) {
     if (db_user.account_type_ == 1) {
       user = new account::Admin();
